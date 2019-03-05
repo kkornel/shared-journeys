dfbb6ca5337cbfe7b940245ca6480c7f4e8a4612
Kornel <kkorneel1@gmail.com>
2019-03-05 09:39:05 +0100

    Start and destination

--- app/src/main/java/com/put/miasi/MainActivity.java
+++ app/src/main/java/com/put/miasi/MainActivity.java
@@ -3,13 +3,23 @@ package com.put.miasi;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
@@ -25,35 +35,51 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final String TAG = "MainActivity";
+
+    private static final String API_KEY = "AIzaSyC7Odnoubb_yC1QvUiy0AOm8eafjJUx-UE";
+
+    private static final float ZOOM_LEVEL = 17.0f;
+    private static final int MARKER_MAP_PADDING = 200;
+
+    private GoogleMap mMap;
+
+    private Button routeButton;
+
+    private LatLng startLatLng;
+    private LatLng destLatLng;
+
+    private List<Marker> mMarkersList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-// Initialize Places.
-        Places.initialize(getApplicationContext(),  "AIzaSyC7Odnoubb_yC1QvUiy0AOm8eafjJUx-UE");
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-// Create a new Places client instance.
+        // Initialize Places.
+        Places.initialize(getApplicationContext(), API_KEY);
+
+        // Create a new Places client instance.
         final PlacesClient placesClient = Places.createClient(this);
 
         // Initialize the AutocompleteSupportFragment.
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
-// Specify the types of place data to return.
+        // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
 
-// Set up a PlaceSelectionListener to handle the response.
+        // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i("Beep", "Place: " + place.getName() + ", " + place.getId());
-                Log.i("Beep", "Place: " + place.getAddress() + ", " + place.getLatLng());
-
-
+                Log.d(TAG, "Place: " + place.getAddress() + ", " + place.getLatLng());
 
                 String location = place.getName();
                 List<Address> addressList = null;
@@ -66,22 +92,96 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Address address = addressList.get(0);
-                Address address1 = new Address(Locale.getDefault());
 
                 LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
 
-                Log.d("Beep", "onCreate1: " + address.toString());
-                Log.d("Beep", "onCreate1: " + latLng.toString());
+                Log.d(TAG, "onPlaceSelected: " + address.toString());
+                Log.d(TAG, "onPlaceSelected: " + latLng.toString());
+
+                startLatLng = latLng;
             }
 
             @Override
             public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
+                Log.d(TAG, "An error occurred: " + status);
             }
         });
 
+        AutocompleteSupportFragment autocompleteFragmentDest = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment_dest);
 
+        // Specify the types of place data to return.
+        autocompleteFragmentDest.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragmentDest.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.d(TAG, "Place: " + place.getAddress() + ", " + place.getLatLng());
+
+                String location = place.getName();
+                List<Address> addressList = null;
+
+                Geocoder geocoder = new Geocoder(MainActivity.this);
+                try {
+                    addressList = geocoder.getFromLocationName(location, 1);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address address = addressList.get(0);
+
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                Log.d(TAG, "onPlaceSelected: " + address.toString());
+                Log.d(TAG, "onPlaceSelected: " + latLng.toString());
+
+                destLatLng = latLng;
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.d(TAG, "An error occurred: " + status);
+            }
+        });
+
+        routeButton = findViewById(R.id.route_button);
+        routeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (startLatLng == null  || destLatLng == null) {
+                    return;
+                }
+
+                Marker startMarker = mMap.addMarker(new MarkerOptions()
+                        .position(startLatLng)
+                        .title("Start"));
+
+                Marker destMarker = mMap.addMarker(new MarkerOptions()
+                        .position(destLatLng)
+                        .title("Destination"));
+
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+
+                builder.include(startMarker.getPosition());
+                builder.include(destMarker.getPosition());
+
+                LatLngBounds bounds = builder.build();
+                mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, MARKER_MAP_PADDING));
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    private void geoCode() {
         String location = "Poznan";
         List<Address> addressList = null;
 
@@ -97,6 +197,5 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: " + address.toString());
         Log.d(TAG, "onCreate: " + latLng.toString());
-
     }
 }
