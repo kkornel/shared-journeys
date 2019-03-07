f6018ae0e2e3f6ce5afe003ad2e0f69ae80e4ad2
Kornel <kkorneel1@gmail.com>
2019-03-07 17:17:33 +0100

    Fixed offeredRides

--- app/src/main/java/com/put/miasi/utils/Database.java
+++ app/src/main/java/com/put/miasi/utils/Database.java
@@ -2,7 +2,7 @@ package com.put.miasi.utils;
 
 public class Database {
     public static final String USERS = "users";
-    public static final String OFFERED_RIDES = "offered_rides";
+    public static final String OFFERED_RIDES = "offeredRides";
     public static final String RIDES = "rides";
 
     public static final String STORAGE_AVATARS = "avatars";
