832d00a07845779008732383268e193108ef8512
Kornel <kkorneel1@gmail.com>
2019-03-15 08:41:55 +0100

    RideLocationActivity

--- app/src/main/java/com/put/miasi/main/search/RideLocationActivity.java
+++ app/src/main/java/com/put/miasi/main/search/RideLocationActivity.java
@@ -1,15 +1,47 @@
 package com.put.miasi.main.search;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.put.miasi.R;
+import com.put.miasi.main.offer.OfferSummaryActivity;
+import com.put.miasi.utils.DateUtils;
+import com.put.miasi.utils.FetchURL;
+import com.put.miasi.utils.LocationUtils;
+import com.put.miasi.utils.OfferLog;
 import com.put.miasi.utils.RideOffer;
+import com.put.miasi.utils.TaskLoadedCallback;
 
 import static com.put.miasi.main.offer.FromActivity.RIDE_OFFER_INTENT;
 
-public class RideLocationActivity extends AppCompatActivity {
+public class RideLocationActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
+    private static final String TAG = "RideLocationActivity";
+
+    private static final int MARKER_MAP_PADDING = 200;
+
+    private GoogleMap mMap;
+    private Polyline currentPolyline;
+
     private RideOffer mRideOffer;
+
+    private LatLng mStartLatLng;
+    private LatLng mDestinationLatLng;
+    private LatLng mYouLatLng;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +51,85 @@ public class RideLocationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.title_activity_from));
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
         mRideOffer = (RideOffer) getIntent().getSerializableExtra(RIDE_OFFER_INTENT);
         mRideOffer.getStartPoint().getLatitude();
 
+
+        mStartLatLng = mRideOffer.getStartPoint().toLatLng();
+        mDestinationLatLng =  mRideOffer.getDestinationPoint().toLatLng();
+        mYouLatLng = new LatLng(LocationUtils.lastKnowLocation.getLatitude(), LocationUtils.lastKnowLocation.getLongitude());
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+
+
+        Marker startMarker = mMap.addMarker(new MarkerOptions()
+                .position(mStartLatLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .title("Start"));
+
+        Marker destMarker = mMap.addMarker(new MarkerOptions()
+                .position(mDestinationLatLng)
+                .title("Destination"));
+
+        Marker youMarker = mMap.addMarker(new MarkerOptions()
+                .position(mYouLatLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                .title("You"));
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        builder.include(startMarker.getPosition());
+        // builder.include(destMarker.getPosition());
+        builder.include(youMarker.getPosition());
+
+        LatLngBounds bounds = builder.build();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, MARKER_MAP_PADDING));
+
+        new FetchURL(RideLocationActivity.this).execute(getUrl(mStartLatLng, mDestinationLatLng, "driving"), "driving");
+    }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        OfferLog.d(url);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
